eb2b337305df54afe14c19466a6dc9626515e789
Burak Özkan <burak.ozkan@path.com.tr>
2021-11-18 23:25:35 +0300

    episode3

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const tasks = require('./routes/tasks');
+const conncectDB = require('./db/connect');
+require('dotenv').config();
 
 const app = express();
 
@@ -13,14 +15,21 @@ app.get('/', (req,res)=> {
 
 app.use('/api/v1/tasks', tasks);
 
+const PORT = 5001;
+const connectionString = process.env.MONGO_DB_URI;
+
+const start = async () => {
+  try {
+    await conncectDB(connectionString);
+    app.listen(PORT, () => console.log('Server started at port 5000'));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
+
 // app.get('/api/v1/tasks') //get all tasks
 // app.get('/api/v1/tasks/:id') //get a task
 // app.post('/api/v1/tasks') //create a task
 // app.patch('/api/v1/tasks/:id') //update a task
 // app.delete('/api/v1/tasks/:id') //delete a task
-
-
-const PORT = 5001;
-app.listen(PORT, ()=> {
-  console.log(`Server ${PORT} portunda çalışıyor`);
-});
