064839b717f3e997f60d61ae9bc1884bd2b8193a
Burak Özkan <burak.ozkan@path.com.tr>
2021-12-03 00:13:30 +0300

    episode11

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -28,9 +28,9 @@ const getTask = asyncWrapper( async (req, res) => {
 });
 
 
-const deleteTask = async (req, res) => {
+const deleteTask = asyncWrapper ( async (req, res) => {
   
-  try {
+  
     const {id: taskID} = req.params;
     const task = await Task.findByIdAndDelete({_id:taskID}); 
     
@@ -40,15 +40,13 @@ const deleteTask = async (req, res) => {
     
     res.status(200).json({task});
     
-  }catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+ 
   
   res.send('task deleted');
-};
+});
 
-const updateTask = async (req, res) => {
-  try {
+const updateTask = asyncWrapper( async (req, res) => {
+  
     const {id: taskID} = req.params;
     //const {title, description, completed} = req.body;
     const task = await Task.findByIdAndUpdate({_id:taskID}, req.body, {new: true, runValidators: true});
@@ -57,13 +55,11 @@ const updateTask = async (req, res) => {
     if(!task) {
       return res.status(404).json({error: `No task with id : ${taskID}`});
     }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+  
+});
 
-const editTask = async (req, res) => {
-  try {
+const editTask = asyncWrapper( async (req, res) => {
+  
     const {id: taskID} = req.params;
     //const {title, description, completed} = req.body;
     const task = await Task.findByIdAndUpdate({_id:taskID}, req.body, {new: true, runValidators: true, overwrite: true});
@@ -72,10 +68,8 @@ const editTask = async (req, res) => {
     if(!task) {
       return res.status(404).json({error: `No task with id : ${taskID}`});
     }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+  
+});
 
 module.exports = {
   getAllTasks,
